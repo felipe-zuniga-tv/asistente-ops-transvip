5cf2b5c44c0bc677a8f70902c880eb1d32c8fb74
felipe-zuniga-tv <felipe.zuniga@transvip.cl>
2024-09-26 11:11:37 -0300

    responsive design

--- components/qr/qr-code-generator.tsx
+++ components/qr/qr-code-generator.tsx
@@ -40,46 +40,48 @@ export const QRCodeGenerator: React.FC<QRCodeGeneratorProps> = () => {
     };
 
     return (
-        <div className="max-w-2xl mx-auto mt-10 bg-white rounded-lg shadow-lg flex flex-col gap-6 px-6 pb-4">
-            <div className="flex flex-col gap-2 md:gap-4 justify-center items-center px-6 md:px-8 pt-6 md:pt-8">
-                <Image src="/images/transvip-logo-only-color.png"
-                    alt="Transvip Logo"
-                    height={80}
-                    width={80}
-                    className="h-8 w-8 md:h-10 md:w-10" />
-                <div id="welcome-title" className="text-lg md:text-2xl font-bold text-gray-800">Genera tu código QR</div>
-            </div>
-
-            <div className='flex justify-center items-center gap-4'> 
-                <input type="text"
-                    value={bookingNumber}
-                    onChange={(e) => setBookingNumber(e.target.value)}
-                    placeholder="Ingresa el número de reserva"
-                    className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
-                />
-                <Button variant={"default"} onClick={generateQRCode}
-                    className="mx-auto px-4 py-2 bg-orange-500 text-white rounded-md hover:bg-orange-600 transition duration-300 min-w-fit"
-                >
-                    Generar QR
-                </Button>
-            </div>
-
-            <div className='flex justify-start'>
-                <Button variant="default" onClick={handleStartOver}
-                    className="px-4 py-2 bg-gray-500 text-white text-xs rounded-md hover:bg-gray-600 transition duration-300 min-w-fit"
-                >
-                    Empezar de nuevo
-                    <RotateCw className='ml-2 h-4 w-4' />
-                </Button>
-            </div>
-
-            { errorMessage && <div className='text-center text-red-500'>{ errorMessage }</div>}
-
-            {isQrVisible && qrCodeUrl && (
-                <div className="my-6 mx-auto">
-                    <Image height={400} width={400} src={qrCodeUrl} alt='Código QR Generado' className="mx-auto" />
+        <div className='p-4'>
+            <div className="max-w-2xl mx-auto sm:mt-10 bg-white rounded-lg shadow-lg flex flex-col gap-6 px-6 pb-4">
+                <div className="flex flex-col gap-2 md:gap-4 justify-center items-center px-6 md:px-8 pt-6 md:pt-8">
+                    <Image src="/images/transvip-logo-only-color.png"
+                        alt="Transvip Logo"
+                        height={80}
+                        width={80}
+                        className="h-8 w-8 md:h-10 md:w-10" />
+                    <div id="welcome-title" className="text-lg md:text-2xl font-bold text-gray-800">Genera tu código QR</div>
                 </div>
-            )}
+
+                <div className='flex flex-col sm:flex-row justify-center items-center gap-4'> 
+                    <input type="text"
+                        value={bookingNumber}
+                        onChange={(e) => setBookingNumber(e.target.value)}
+                        placeholder="Ingresa el número de reserva"
+                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                    />
+                    <Button variant={"default"} onClick={generateQRCode}
+                        className="mx-auto px-4 py-2 bg-orange-500 text-white rounded-md hover:bg-orange-600 transition duration-300 w-[120px] sm:min-w-fit"
+                    >
+                        Generar QR
+                    </Button>
+                </div>
+
+                <div className='flex justify-start mt-4'>
+                    <Button variant="default" onClick={handleStartOver}
+                        className="px-4 py-2 bg-gray-500 text-white text-xs rounded-md hover:bg-gray-600 transition duration-300 min-w-fit"
+                    >
+                        Empezar de nuevo
+                        <RotateCw className='ml-2 h-4 w-4' />
+                    </Button>
+                </div>
+
+                { errorMessage && <div className='text-center text-red-500'>{ errorMessage }</div>}
+
+                {isQrVisible && qrCodeUrl && (
+                    <div className="my-6 mx-auto">
+                        <Image height={400} width={400} src={qrCodeUrl} alt='Código QR Generado' className="mx-auto" />
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
