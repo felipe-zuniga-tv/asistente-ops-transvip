562cf4a6f53ab2876b8c0cbd0fc3c013ccc63e61
felipe-zuniga-tv <felipe.zuniga@transvip.cl>
2025-01-11 07:58:51 -0300

    .

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
-import { isSessionExpired, updateSession } from "./lib/auth";
-import { Routes } from "./utils/routes";
+import { updateSession } from "./lib/auth";
 
 export async function middleware(request: NextRequest) {
   try {
-    if (await isSessionExpired(request)) {
-      return NextResponse.redirect(new URL(Routes.HOME, request.url));
-    }
-
     return await updateSession(request);
   } catch (error) {
     console.error('Middleware session update error:', error);
