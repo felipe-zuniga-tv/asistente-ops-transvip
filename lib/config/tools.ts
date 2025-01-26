b9117c8ff72facd0670a94bda7ca3ab25156b820
felipe-zuniga-tv <felipe.zuniga@transvip.cl>
2025-01-26 10:56:08 -0300

    .

--- lib/config/tools.ts
+++ lib/config/tools.ts
@@ -1,5 +1,5 @@
 import { Car, CarIcon, CarTaxiFrontIcon, Clock12Icon, FileDigitIcon, PlaneTakeoffIcon, User, UserSearch } from "lucide-react";
-import { Tool } from "../chat/types";
+import { Tool } from "@/lib/types";
 
 // TOOLS
 export const toolsList: Tool[] = [
