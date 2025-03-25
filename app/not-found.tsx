51d6e874da57ac24058f42542814323e5cc3a159
felipe-zuniga-tv <felipe.zuniga@transvip.cl>
2025-03-25 15:24:41 -0300

    .

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,23 +1,25 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Routes } from "@/utils/routes";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { HomeIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Routes } from "@/utils/routes";
 
-export default function NotFoundComponent(): JSX.Element {
+export default function NotFoundComponent() {
 	const path = usePathname();
 
 	return (
-		<div className="bg-main min-h-screen flex flex-col items-center justify-center px-4 md:px-0">
+		<div className="bg-main bg-cover bg-top min-h-screen flex flex-col items-center justify-center px-4 md:px-0">
 			<div className="p-24 rounded-xl text-lg bg-white/90 text-center gap-4 flex flex-col">
 				<h1 className="text-xl font-semibold">¡Hola!</h1>
-				<p className="text-lg font-normal">
+				<span className="text-lg font-normal">
 					No pudimos encontrar la ruta {`"${path}"`}
-				</p>
-				<Link href={Routes.HOME} className="mt-4">
+				</span>
+				<Link href={Routes.HOME}>
 					<Button className="w-full bg-transvip hover:bg-transvip-dark py-4 text-lg h-12">
-						Inicio
+						<HomeIcon className="w-4 h-4" />
+						Volver al inicio
 					</Button>
 				</Link>
 			</div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import LoginFormServer from "@/components/auth/login-server";
 import { TransvipLogo } from "@/components/transvip/transvip-logo";
 
 export const metadata: Metadata = {
-	title: "Iniciar Sesión | Transvip Operaciones",
-	description: "Portal de inicio de sesión para operaciones Transvip Chile",
-};
+	title: "Transvip | Operaciones | Iniciar Sesión",
+	description: "Transvip Chile | Portal de operaciones",
+}
 
-export default function Home(): JSX.Element {
+export default function Home() {
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center p-4 bg-main bg-cover bg-top">
 			<div className="w-full max-w-lg bg-white rounded-lg">
