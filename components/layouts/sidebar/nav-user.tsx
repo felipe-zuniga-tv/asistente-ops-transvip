e9f0598542a0845dfb3799e9da2897bc73ea6dc9
felipe-zuniga-tv <felipe.zuniga@transvip.cl>
2025-04-12 01:43:58 -0700

    .

--- components/layouts/sidebar/nav-user.tsx
+++ components/layouts/sidebar/nav-user.tsx
@@ -38,11 +38,11 @@ export function NavUser({ user }: ChatSession) {
 							className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
 						>
 							<Avatar className="h-8 w-8 rounded-lg">
-								<AvatarImage src={'/images/transvip-logo-only-color.png'} alt={user.fullName} className="p-0.5" />
-								<AvatarFallback className="rounded-full bg-slate-700 text-white">{getInitials(user.fullName || "")}</AvatarFallback>
+								<AvatarImage src={'/images/transvip-logo-only-color.png'} alt={user.full_name} className="p-0.5" />
+								<AvatarFallback className="rounded-full bg-slate-700 text-white">{getInitials(user.full_name || "")}</AvatarFallback>
 							</Avatar>
 							<div className="grid flex-1 text-left text-sm leading-tight">
-								<span className="truncate font-semibold">{user.fullName}</span>
+								<span className="truncate font-semibold">{user.full_name}</span>
 								<span className="truncate text-xs">{user.email}</span>
 							</div>
 							<ChevronsUpDown className="ml-auto size-4" />
@@ -57,11 +57,11 @@ export function NavUser({ user }: ChatSession) {
 						<DropdownMenuLabel className="p-0 font-normal">
 							<div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
 								<Avatar className="h-8 w-8 rounded-lg">
-									<AvatarImage src={'/images/transvip-logo-only-color.png'} alt={user.fullName} className="p-0.5" />
-									<AvatarFallback className="rounded-full bg-slate-700 text-white">{getInitials(user.fullName || "")}</AvatarFallback>
+									<AvatarImage src={'/images/transvip-logo-only-color.png'} alt={user.full_name} className="p-0.5" />
+									<AvatarFallback className="rounded-full bg-slate-700 text-white">{getInitials(user.full_name || "")}</AvatarFallback>
 								</Avatar>
 								<div className="grid flex-1 text-left text-sm leading-tight">
-									<span className="truncate font-semibold">{user.fullName}</span>
+									<span className="truncate font-semibold">{user.full_name}</span>
 									<span className="truncate text-xs">{user.email}</span>
 								</div>
 							</div>
