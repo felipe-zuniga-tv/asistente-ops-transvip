47b46ad4573cc39e10c3bda11103e6fd4b081c08
felipe-zuniga-tv <felipe.zuniga@transvip.cl>
2025-05-22 00:29:01 -0400

    refactor #20

--- components/features/vehicle-shifts/definition/delete-shift-alert-dialog.tsx
+++ components/features/vehicle-shifts/definition/delete-shift-alert-dialog.tsx
@@ -10,7 +10,7 @@ import {
 	AlertDialogHeader,
 	AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
-import { Shift } from "./shifts-definition";
+import type { Shift } from "@/types/domain/shifts/types";
 
 const WEEKDAYS = {
 	1: "Lunes",
